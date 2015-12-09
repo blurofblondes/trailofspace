ce7eb86cfa831864a1bdc84fb85b0052220a4d8b
blurofblondes <blurofblondes@gmail.com>
2015-12-08 17:13:00 -0700

    10/08/2015 - Initial commit to repository

--- src/trailofspace/TrailofSpace.java
+++ src/trailofspace/TrailofSpace.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package trailofspace;
+package trailofspace; 
 
 
 import byui.cit260.curiousWorkmanship.model.Player;
